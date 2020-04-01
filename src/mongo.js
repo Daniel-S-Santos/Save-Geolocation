bbab521aca096a1dc6a19720e4b417d131ff5d73
Sistemas GrupoABL <sistemas@gabl.lan>
2020-04-01 14:56:18 -0300

    asd

--- src/mongo.js
+++ src/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-
+const mongodb = require('mongodb')
 // mongoose.connect("geonosis.mongodb.umbler.com:46115",{ useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.connect('mongodb://nothin04:danielk20@mongo_amey:27017/Amey')
+mongodb.connect('mongodb://nothin04:danielk20@mongo_amey:27017/Amey')
 var db = mongoose.connection
 
 db.once('connected', ()=>{
