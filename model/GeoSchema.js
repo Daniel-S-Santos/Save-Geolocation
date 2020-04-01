145be66131a1426083cf92ce35f2e2ff5ebb2cf7
Sistemas GrupoABL <sistemas@gabl.lan>
2020-04-01 16:13:17 -0300

    asd

--- model/GeoSchema.js
+++ model/GeoSchema.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose')
 const mongodb = require('mongodb')
-const GeoSchema = new mongodb.Schema({
+
+const GeoSchema = mongoose.Schema({
     latitude: {type: String},
     longitude: {type: String},
     location: {type: String},
     createdAt: {type: Date, default:Date.now }
 })
-// const GeoSchema = mongoose.Schema({
-//     latitude: {type: String},
-//     longitude: {type: String},
-//     location: {type: String},
-//     createdAt: {type: Date, default:Date.now }
-// })
 
-var Geo = mongodb.model("Geo",GeoSchema)
+var Geo = mongoose.model("Geo",GeoSchema)
 
 module.exports = Geo
 
