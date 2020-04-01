2e1130d1bfca1618111ef14ee5f25d6e854ac5ea
Sistemas GrupoABL <sistemas@gabl.lan>
2020-04-01 16:22:09 -0300

    asd

--- src/mongo.js
+++ src/mongo.js
@@ -7,7 +7,7 @@ uri = "mongodb://nothin04:danielk20@mongo_amey:27017/amey?retryWrites=true&w=maj
 // mongoDB.connect(uri,{ useUnifiedTopology: true }).then(()=>{
 //     console.log('deu bom')
 // }) 
-mongoose.connect(uri,{ useUnifiedTopology: true }).then(()=>{
+mongoose.connect(uri,{ useUnifiedTopology: true, useNewUrlParser: true }).then(()=>{
     console.log('deu bom')
 })
 //mongodb.connect('mongodb://nothin04:danielk20@mongo_amey:27017/amey',{ useUnifiedTopology: true})
