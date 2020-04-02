447bd160e43b5f1eb01ab674fc8bf5b6473d9af5
Sistemas GrupoABL <sistemas@gabl.lan>
2020-04-02 17:07:06 -0300

    troca de banco

--- src/mongo.js
+++ src/mongo.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 
 
 
-//uri = "mongodb://nothin04:danielk20@mongo_amey:27017/amey?retryWrites=true&w=majority";
-uri = "mongodb://localhost:27017/Amey"
+uri = "mongodb://nothin04:danielk20@mongo_amey:27017/amey?retryWrites=true&w=majority";
+//uri = "mongodb://localhost:27017/Amey"
 // mongoDB.connect(uri,{ useUnifiedTopology: true }).then(()=>{
 //     console.log('deu bom')
 // }) 
